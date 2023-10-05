0abb729f293a21835266c1e45edf2631b86d97d7
vinay-parekh <bvp0802@gmail.com>
2023-10-05 19:19:10 +0530

    first commit

---
+++ src/main/java/com/example/projectmgmt/ProjectmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectmgmt/ProjectmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
